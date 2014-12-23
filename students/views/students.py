52090cd9ba0be062be3d0f8487d91e34afd5ea4d
max1902 <prokazamax@gmail.com>
2014-12-24 00:21:42 +0200

    add new new all new

--- students/views/students.py
+++ students/views/students.py
@@ -3,29 +3,15 @@
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from ..models import Student
+
 
 
 # Views for Students
 
 
 def students_list(request):
-    students = (
-        {'id': 1,
-         'first_name': u'Андрій',
-         'last_name': u'Корост',
-         'ticket': 2123,
-         'image': 'img/1.jpg'},
-        {'id': 2,
-         'first_name': u'Максим',
-         'last_name': u'Проказа',
-         'ticket': 2000,
-         'image': 'img/2.jpg'},
-        {'id': 3,
-         'first_name': u'Ольга',
-         'last_name': u'Яроменко',
-         'ticket': 2050,
-         'image': 'img/3.jpg'},
-    )
+    students = Student.objects.all()
     return render(request, 'students/students_list.html', {'students':students})
 
 def students_add(request):
--- studentsdb/urls.py
+++ studentsdb/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from .settings import MEDIA_ROOT, DEBUG
 
 
 urlpatterns = patterns('',
@@ -32,4 +33,13 @@ urlpatterns = patterns('',
     
 
     url(r'^admin/', include(admin.site.urls)),
+
+
+
 )
+
+if DEBUG:
+    # serve files from media folder
+    urlpatterns += patterns('',
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+     {'document_root': MEDIA_ROOT}))
