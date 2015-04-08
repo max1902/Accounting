3a8405b140b4791ec0b686fbf88523c9dec2f590
max1902 <prokazamax@gmail.com>
2015-04-08 13:06:18 +0300

    deploy heroku

--- studentsdb/settings.py
+++ studentsdb/settings.py
@@ -84,7 +84,10 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static'),
+    )
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, '..', 'media')
 
