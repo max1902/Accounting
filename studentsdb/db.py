541b97bbf7a129219a6dd46a8e2ec75d98a3795d
max1902 <prokazamax@gmail.com>
2015-04-09 13:16:16 +0300

    deploy heroku

--- studentsdb/db.py
+++ studentsdb/db.py
@@ -4,7 +4,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'HOST': 'localhost',
+        'HOST': 'https://mystudentdb.herokuapp.com/',
         'USER': 'students_db_user',
         'PASSWORD': '111111',
         'NAME': 'students_db',
